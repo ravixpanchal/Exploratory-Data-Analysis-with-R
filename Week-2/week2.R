f9739cd0ea0e3932a955043f19286c06b170f110
ravixpanchal <ravi.panchal.kaithi@gmail.com>
2026-01-24 15:03:06 +0530

    week 2 completed

--- Week-2/week2.R
+++ Week-2/week2.R
@@ -83,3 +83,46 @@ subset(painters, School=="F", select=c(-3,-5))
 
 #Split
 splitted=split(painters,painters$School)
+
+#Absolute and Relative frequencies
+result=c(1,2,1,2,2,1,1,2,1,1)
+result
+
+table(result) #Absolute frequency
+table(result)/length(result) #relative frequency
+
+#Example
+library(MASS)
+colnames(painters)
+painters$School
+
+table(painters$School) #Absolute frequency
+table(painters$School)/length(painters$School) #Relative frequency
+
+#Cumulative frequency
+result=c(1,2,1,2,2,1,1,2,1,1)
+table(result)
+
+cumsum(table(result)) #cumulative frequency (less than type)
+
+#Example
+library(MASS)
+table(painters$School)
+cumsum(table(painters$School)) #absolute frequency
+cumsum(table(painters$School)/length(painters$School)) #relative frequency
+
+#frequency distribution
+time=c(32,35,45,83,74,55,68,38,35,55,66,65,42,68,72,84,67,36,42,58)
+time
+range(time) #find the min and max value
+breaks=seq(30,90,by=10) #seq at interval of 10 integers
+time.cut=cut(time,breaks,right=FALSE)
+time.cut
+table(time.cut)
+cbind(table(time.cut))
+table(time.cut)/length(time.cut)
+cbind(table(time.cut)/length(time.cut))
+cumsum(table(time.cut))
+cbind(cumsum(table(time.cut)))
+cumsum(table(time.cut)/length(time.cut))
+cbind(cumsum(table(time.cut)/length(time.cut)))
---
+++ code.R
@@ -0,0 +1,3 @@
+x=4.3
+print(class(x))
+print(typeof(x))
